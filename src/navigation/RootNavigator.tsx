3429bd9f3761750907fa263b8de805921dda2e28
ryannh <ryannh30@gmail.com>
2025-10-29 13:41:18 +0700

    feat: :sparkles: create RootNavigator and impl in App.tsx file

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,25 @@
+import { enableScreens } from 'react-native-screens';
+enableScreens(true);
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetailScreen from '../screens/DetailScreen';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Detail: { id: number };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
